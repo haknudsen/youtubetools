84363461f0477188540b6b02f95d94503adb72f7
Andy Knudsen <andy@websitetalkingheads.com>
2017-07-16 18:13:58 -0600

    added footer to tags.php

--- tags.php
+++ tags.php
@@ -142,14 +142,16 @@ END;
 END;
 }
 ?>
-
 <!doctype html>
 <html>
-
 <head>
-    <title>Video Tags</title>
-    <link rel="stylesheet" href="../dist/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="my_uploads.css">
+<title>Get Tags</title>
+</head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="my_uploads.css">
 </head>
 
 <body>
