3cee1d7c5452a5f7d372710835f562eac44257d5
Andy Knudsen <andy@websitetalkingheads.com>
2017-08-01 01:08:09 -0600

    started on description editor

--- includes/header-autoresize.js
+++ includes/header-autoresize.js
@@ -3,8 +3,14 @@ $.fn.extend({
     autoresize: function () {
         "use strict";
         $(this).on('change keyup keydown paste cut blur', function () {
-            $(this).height(0).height(this.scrollHeight);
-            console.log('Hit ' + this);
+        var newHeight =  this.scrollHeight;
+            if(this.scrollHeight < 40){
+                newHeight = 40;
+            }
+            if(this.scrollHeight > 800){
+                newHeight = 800;
+            }
+            $(this).height(0).height(newHeight);
         }).change();
     }
 });
