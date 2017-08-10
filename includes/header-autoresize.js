1a68f32e91f8c31ccc2027f2d4940ad02d48022a
haknudsen <paris_giovanni@yahoo.com>
2017-08-10 12:03:57 -0600

    updated list to spin to include to commas for youtube keywords
    updated nav to reflect the change

--- includes/header-autoresize.js
+++ includes/header-autoresize.js
@@ -1,21 +1,4 @@
 // JavaScript Document
-$.fn.extend({
-    autoresize: function () {
-        "use strict";
-        $(this).on('change paste cut blur', function () {
-        var newHeight =  this.scrollHeight;
-            console.log('newHeight-' + newHeight);
-            console.log('current Height-' +$(this).height());
-            if(this.scrollHeight < 40){
-                newHeight = 40;
-            }
-            if(this.scrollHeight > 800){
-                newHeight = 800;
-            }
-            $(this).height(0).height(newHeight);
-        }).change();
-    }
-});
     $(document).ready(function(){
         "use strict";
         $("#footer").load("includes/nav.html");
