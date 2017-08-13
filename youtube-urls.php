89249286a75231b3ffe0b7ace664010af4ce2532
Andy Knudsen <andy@websitetalkingheads.com>
2017-08-13 14:03:42 -0600

    modified max height for textareas

--- youtube-urls.php
+++ youtube-urls.php
@@ -15,54 +15,56 @@ $response = curl_exec( $ch );
 ?>
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>Get YouTube Video URL Variations</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" href="css/my_uploads.css">
+    <meta charset="utf-8">
+    <title>Get YouTube Video URL Variations</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/my_uploads.css">
 </head>
 
 <body>
-<div id="header"></div>
-<section class="jumbotron text-center">
-  <h1>Get YouTube Video URL Variations</h1>
-  <div class="container text-center">
-    <div class="row">
-      <div class="col-sm-6">
-       <h2>videoID:</h2> 
-        <input type="text" id="videoID">
-      </div>
-      <div class="col-sm-6">
-       <h2>Keywords(one per row):</h2> 
-          <textarea id="keywords"></textarea>
-      </div>
-        <button type="button" class="btn btn-purple center" id="getURLs">Get URLs</button>
-    </div>
-  </div>
-  <div class="alert alert-info">
-    <div class="row">
-      <div class="col-sm-12">
-       <form action="includes/pastebin-send.php">
-          <button type="submit" class="btn btn-green">Send to Paste Bin</button>
-          <div class="m-1">
-              <label>Enter name for Pastebin:<input type="text" value="" name="playlistID" id="playlistID"></label>
-          </div>
-        <textarea id="link-container" name="link-container" class="text-left"></textarea>
-          <input type="hidden" value="<?=$response?>" name="userID" id="userID">
-         </form>
-      </div>
-    </div>
-    </div>
-</section>
-<footer id="footer"></footer>
-<script src="includes/header-autoresize.js"></script> 
-<script src="includes/youtube_variations.js"></script> 
-<script src="includes/autosize.js"></script>
-<script>
-    autosize(document.querySelectorAll('textarea'));
+    <div id="header"></div>
+    <section class="jumbotron text-center">
+        <h1>Get YouTube Video URL Variations</h1>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>videoID:</h2>
+                    <input type="text" id="videoID">
+                </div>
+                <div class="col-sm-6">
+                    <h2>Keywords(one per row):</h2>
+                    <textarea id="keywords" style="max-height: 500px"></textarea>
+                </div>
+                <button type="button" class="btn btn-purple center" id="getURLs">Get URLs</button>
+            </div>
+        </div>
+        <div class="alert alert-info">
+            <div class="row">
+                <div class="col-sm-12">
+                    <form action="includes/pastebin-send.php">
+                        <button type="submit" class="btn btn-green">Send to Paste Bin</button>
+                        <div class="m-1">
+                            <label>Enter name for Pastebin:<input type="text" value="" name="playlistID" id="playlistID"></label>
+                        </div>
+                        <textarea id="link-container" name="link-container" class="text-left" style="max-height: 500px"></textarea>
+                        <input type="hidden" value="<?=$response?>" name="userID" id="userID">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer id="footer"></footer>
+    <script src="includes/header-autoresize.js"></script>
+    <script src="includes/youtube_variations.js"></script>
+    <script src="includes/autosize.js"></script>
+    <script>
+        autosize( document.querySelectorAll( 'textarea' ) );
     </script>
 </body>
+
 </html>
