5792fbdab4b1e523103b06a0e288314b157fc3a7
Andy Knudsen <andy@websitetalkingheads.com>
2017-09-05 00:25:30 -0600

    Added Alphabetize to list function

--- includes/lists-make.js
+++ includes/lists-make.js
@@ -1,52 +1,69 @@
 // JavaScript Document
-
-$("#getSpintax").click(function () {
+function listFunctions() {
     "use strict";
-    var list = $("#decode").val();
-    var text = list.replace(/(\r\n|\n|\r)/gm, "|");
-    text = "{" + text;
-    text += "}";
-    $("#spin").val(text);
-    fieldUpdate();
-});
-$("#commas").click(function () {
-    "use strict";
-    var list = $("#decode").val();
-    var text = list.replace(/(\r\n|\n|\r)/gm, ",");
-    $("#spin").val(text);
-    fieldUpdate();
-});
-$("#getHTML").click(function () {
-    "use strict";
-    var list = $("#decode").val();
-    var text = list.replace(/(\r\n|\n|\r)/gm, ",");
-    text = text.split(',');
-    var i = 0, holder = "";
-    $("#spin").val('');
-    while(text[i]){
-        holder += '<li>' + text[i] + '</li>' + '\n';
-        i++;
-    }
-    $("#spin").val(holder);
-    fieldUpdate();
-});
-$('#clear').click(function(){
-    "use strict";
-    $("#spin").val('');
-    $("#decode").val('');
-    fieldUpdate();
-});
-$(document).ready(function () {
-    "use strict";
+    var list, text, i, holder;
+    $("#getSpintax").click(function () {
+        list = $("#decode").val();
+        text = list.replace(/(\r\n|\n|\r)/gm, "|");
+        text = "{" + text;
+        text += "}";
+        $("#spin").val(text);
+        i = 1;
+        fieldUpdate();
+    });
+    $("#commas").click(function () {
+        list = $("#decode").val();
+        text = list.replace(/(\r\n|\n|\r)/gm, ",");
+        $("#spin").val(text);
+        i = 1;
+        fieldUpdate();
+    });
+    $("#getHTML").click(function () {
+        list = $("#decode").val();
+        text = list.replace(/(\r\n|\n|\r)/gm, ",");
+        text = text.split(',');
+        i = 0;
+        holder = "<ul>\n";
+        $("#spin").val('');
+        while (text[i]) {
+            holder += '   <li>' + text[i] + '</li>' + '\n';
+            i++;
+        }
+        holder += "</ul>";
+        $("#spin").val(holder);
+        fieldUpdate();
+    });
+    $("#alphabetize").click(function () {
+        list = $("#decode").val();
+        text = list.split('\n');
+        text.sort();
+        $("#spin").val('');
+        holder = "";
+        i = 0;
+        while (text[i]) {
+            if (text[i] !== "") {
+                holder += text[i] + '\n';
+            }
+            i++;
+        }
+        $("#spin").val(holder);
+        fieldUpdate();
+    });
+    $('#clear').click(function () {
+        $("#spin").val('');
+        $("#decode").val('');
+        fieldUpdate();
+    });
     $('#spin').simplyCountable({
         counter: '#counter',
         countType: 'characters',
         maxCount: 500
     });
-});
-function fieldUpdate(){
-    "use strict";
-    autosize.update($('#spin'));
-    autosize.update($('#decode'));
-    $('#spin').blur();
+
+    function fieldUpdate() {
+        $('#lines').text(i);
+        autosize.update($('#spin'));
+        autosize.update($('#decode'));
+        $('#spin').blur();
+    }
 }
