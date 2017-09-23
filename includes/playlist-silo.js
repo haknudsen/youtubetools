65496454a0207b77993a61cc9a7dbc232b29ff2f
Andy Knudsen <andy@websitetalkingheads.com>
2017-09-22 19:17:09 -0600

    trying to get silo to work

--- includes/playlist-silo.js
+++ includes/playlist-silo.js
@@ -1,9 +1,9 @@
 // Define some variables used to remember state.
-var playlistId, nextPageToken, prevPageToken;
+var playlistId, nextPageToken, prevPageToken, GoogleAuth, description, snippet, videoId, getVideo, categoryId, isAuthorized, phrase,i,count;
 var videoList = Array();
-var GoogleAuth, title, description, channel, snippet, videoId, getVideo, categoryId,privacy,language,isAuthorized;
-var tags = Array();
 var videoUpdate = true;
+var link = 'https://youtu.be/';
+var counter = 0;
 // After the API loads, call a function to get the uploads playlist ID.
 function handleAPILoaded() {
     "use strict";
@@ -58,32 +58,49 @@ function requestVideoPlaylist(playlistId, pageToken) {
             $.each(playlistItems, function (index, item) {
                 displayResult(item.snippet);
             });
-            $("#reporter").text($("#optionlist option:first").val());
+            $("#reporter").text(videoList);
+            $('#count').text(videoList.length);
+            autosize.update($('#reporter'));
+            videoUpdate = false;
+            i = 0;
+            count = 1;
+            while(videoList[i]){
+                console.log( count );
+            getVideo = videoList[i];
+            defineRequest(getVideo);
+            waitResults();
+                count++;
+                if(count>videoList.length-1){
+                    count=0;
+                }else{
+                setTimeout(function () {
+                        $('#description').val('');
+                    }, 5000);
+                }
+                i++;
+            }
         } else {
             $('#video-container').html('Sorry you have no uploaded videos');
         }
-        $('#optionlist').on('change',function(){
-            console.log( 'hit ' );
-            $('#reporter').text($('#optionlist').val());
-            getResults();
-        });
     });
 }
-//get results
-
-       function getResults() {
-            videoUpdate = false;
-            getVideo = $('#reporter').text();
-            defineRequest(getVideo);
-        }
-        $('#update').click(function () {
-            update();
-        });
+            var waitResults = function () {
+                if ($('#description').val() !== '') {
+                    phrase = $('#phrase').val()+ ' ' + link + videoList[count];
+                    console.log( phrase );
+                    $('#description').val($('#description').val() + '\n' + phrase );
+                } else {
+                    setTimeout(function () {
+                        waitResults();
+                    }, 5000);
+                }
+            };
 // Create a listing for a video.
 function displayResult(videoSnippet) {
     "use strict";
     var videoId = videoSnippet.resourceId.videoId;
-    $('#optionlist').append('<option value="' + videoId + '">' + videoId + '</option>');
+    videoList[counter] = videoId;
+    counter++;
 }
 
 // Retrieve the next page of videos in the playlist.
@@ -96,4 +113,174 @@ function nextPage() {
 function previousPage() {
     "use strict";
     requestVideoPlaylist(playlistId, prevPageToken);
+}
+
+function handleClientLoad() {
+    "use strict";
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    "use strict";
+    // Initialize the gapi.client object, which app uses to make API requests.
+    // Get API key and client ID from API Console.
+    // 'scope' field specifies space-delimited list of access scopes
+
+    gapi.client.init({
+        'clientId': '90875073625-ff3ntj9kehegnbamaoace12q6lo37v6u.apps.googleusercontent.com',
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+        'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
+    }).then(function () {
+        GoogleAuth = gapi.auth2.getAuthInstance();
+
+        // Listen for sign-in state changes.
+        GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+        // Handle initial sign-in state. (Determine if user is already signed in.)
+        setSigninStatus();
+
+        // Call handleAuthClick function when user clicks on "Authorize" button.
+        $('#execute-request-button').click(function () {
+            handleAuthClick(event);
+        });
+    });
+}
+
+function handleAuthClick(event) {
+    "use strict";
+    // Sign user in after click on auth button.
+    GoogleAuth.signIn();
+}
+
+function setSigninStatus() {
+    "use strict";
+    var user = GoogleAuth.currentUser.get();
+    isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
+    // Toggle button text and displayed statement based on current auth status.
+    if (isAuthorized) {}
+}
+
+function updateSigninStatus(isSignedIn) {
+    "use strict";
+    setSigninStatus();
+}
+
+function createResource(properties) {
+    "use strict";
+    var resource = {};
+    var normalizedProps = properties;
+    for (var p in properties) {
+        var value = properties[p];
+        if (p && p.substr(-2, 2) == '[]') {
+            var adjustedName = p.replace('[]', '');
+            if (value) {
+                normalizedProps[adjustedName] = value.split(',');
+            }
+            delete normalizedProps[p];
+        }
+    }
+    for (var p in normalizedProps) {
+        // Leave properties that don't have values out of inserted resource.
+        if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
+            var propArray = p.split('.');
+            var ref = resource;
+            for (var pa = 0; pa < propArray.length; pa++) {
+                var key = propArray[pa];
+                if (pa == propArray.length - 1) {
+                    ref[key] = normalizedProps[p];
+                } else {
+                    ref = ref[key] = ref[key] || {};
+                }
+            }
+        };
+    }
+    return resource;
+}
+
+function removeEmptyParams(params) {
+    "use strict";
+    for (var p in params) {
+        if (!params[p] || params[p] == 'undefined') {
+            delete params[p];
+        }
+    }
+    return params;
+}
+
+function executeRequest(request) {
+    "use strict";
+    request.execute(function (response) {
+        if (videoUpdate === false) {
+            snippet = response.items[0].snippet;
+            categoryId = snippet.categoryId;
+            videoId = response.items[0].id;
+            description = snippet.description;
+            $('#description').val(description);
+            autosize.update($('#description'));
+        }
+    });
+}
+
+function buildApiRequest(requestMethod, path, params, properties) {
+    "use strict";
+    params = removeEmptyParams(params);
+    var request;
+    if (properties) {
+        var resource = createResource(properties);
+        request = gapi.client.request({
+            'body': resource,
+            'method': requestMethod,
+            'path': path,
+            'params': params
+        });
+    } else {
+        request = gapi.client.request({
+            'method': requestMethod,
+            'path': path,
+            'params': params
+        });
+    }
+    executeRequest(request);
+}
+
+/***** END BOILERPLATE CODE *****/
+
+
+function defineRequest(getVideo) {
+    "use strict";
+    // See full sample for buildApiRequest() code, which is not 
+    // specific to a particular youtube or youtube method.
+
+    buildApiRequest('GET',
+        '/youtube/v3/videos', {
+            'id': getVideo,
+            'part': 'snippet'
+        });
+
+}
+
+function update() {
+    "use strict";
+    videoUpdate = true;
+    description = $('#description').val();
+    // Sample js code for videos.update
+
+    // See full sample for buildApiRequest() code, which is not 
+    // specific to a particular youtube or youtube method.
+
+    buildApiRequest('PUT',
+        '/youtube/v3/videos', {
+            'part': 'snippet'
+        }, {
+            'id': videoId,
+            'snippet.categoryId': '19',
+            'snippet.description': description
+        });
+    $('#counter').text('updated!');
+}
+
+function getTags() {
+    "use strict";
+    var stringTags = $('#tags').val();
+    return stringTags.split(',');
 }
