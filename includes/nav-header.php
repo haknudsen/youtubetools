06259ea889a801d5d613bff3582defb715f5775b
Andy Knudsen <andy@websitetalkingheads.com>
2017-09-25 19:06:45 -0600

    cleaning up includes folder

--- includes/nav-header.php
+++
@@ -1,4 +0,0 @@
-<div class="navbar navbar-default my-nav">
- <img src="http://seovideoexperts.com/wp-content/uploads/SEO-Video-Experts.png" alt="SEO Video Experts" class="img-fluid center-block">
-    <?php include('includes-nav.html');?>
-</div>
