1c3e923c968a9c70a9f774263fad8e86272bdd1e
Andy Knudsen <andy@websitetalkingheads.com>
2017-10-13 12:58:14 -0600

    updated header

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-default my-nav">
     <a href="http://seovideoexperts.com/">
-   <img src="../wp-content/uploads/SEO-Video-Experts.jpg" alt="Video SEO is the process of optimizing a site's video content to increase traffic. Video SEO is shortened as vSEO and is an aspect of search engine optimization. A video site map or a video gallery works well as Google uses snippets to aid in the development of results." class="img-fluid center-block" title="SEO Video Experts"/>
+   <img src="http://seovideoexperts.com/wp-content/uploads/SEO-Video-Experts.jpg" alt="Video SEO is the process of optimizing a site's video content to increase traffic. Video SEO is shortened as vSEO and is an aspect of search engine optimization. A video site map or a video gallery works well as Google uses snippets to aid in the development of results." class="img-fluid center-block" title="SEO Video Experts"/>
    </a>
     <?php include('nav.html');?>
 </div>
