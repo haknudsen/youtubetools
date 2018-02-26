8e23e70fd83a121a3aeb0ebd9a40abdd54ec131b
haknudsen <paris_giovanni@yahoo.com>
2018-02-26 13:56:17 -0700

    added responsive embed for playlist

---
+++ includes/playlist-embed-only.js
@@ -0,0 +1,40 @@
+// Define some variables used to remember state.
+var center;
+var anchor = '<div style="width: 100%;max-width:1280px;margin:0 auto;padding:1rem">\n  <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">\n    <iframe type="text/html" style="position: absolute; top:0; left: 0; width: 100%; height: 100%" src="https://www.youtube.com/embed/videoseries?list=';
+var left = '<div style="width: 50%;max-width:640px;float:left;padding-right:1rem">\n  <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">\n    <iframe type="text/html" style="position: absolute; top:0; left: 0; width: 100%; height: 100%" src="https://www.youtube.com/embed/videoseries?list=';
+var frameEnd = '" frameborder="0"></iframe>\n  </div>\n</div>';
+var leftEnd = '\n  </div>\n</div>';
+
+$('#reset').click(function () {
+	"use strict";
+	console.log('reset');
+	$('#playlist').text('');
+	$('#link-container').val('');
+	$('#spintax').val('');
+});
+$('#embed-center').click(function () {
+	"use strict";
+	center = true;
+	getPlaylist();
+});
+$('#responsive').click(function () {
+	"use strict";
+	center = false;
+	getPlaylist();
+});
+
+function getPlaylist() {
+	"use strict";
+	$('#link-container').val('');
+	var playlist = $('#playlist').val();
+	var playlistId = playlist.substring(playlist.lastIndexOf("=") + 1);
+	var info = "";
+	if (center) {
+		info = anchor;
+	} else {
+		info = left;
+	}
+	info += playlistId + frameEnd;
+	$('#link-container').val(info);
+	autosize.update($('#link-container'));
+}
---
+++ playlist-embed.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Get Embed Code for a Playlist</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/my_uploads.css">
+</head>
+
+<body>
+    <div id="header"></div>
+    <section class="container">
+        <h1 class="text-center">Get Responsive Embed Code for a Playlist</h1>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <input type="text" name="playlist" id="playlist" style="width: 800px;margin: 5px auto">
+                <div class="row  text-capitalize center">
+                    <h3 class="text-center">Text Anchors</h3>
+                    <div class="row">
+                        <div class="col-sm-4 col-sm-offset-2">
+                            <button type="button" class="btn btn-drkblue" id="responsive">Left</button>
+                        </div>
+                        <div class="col-sm-4">
+                            <button type="button" class="btn btn-purple" id="embed-center">Center</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="alert alert-success">
+        <textarea class="m-1" id="link-container" style="max-height: 400px"></textarea>
+        <button type="button" class="btn btn-primary text-capitalize center-block" id="copy">Copy</button>
+        <button type="button" class="btn btn-warning text-capitalize center-block" id="reset">Reset</button>
+    </section>
+    
+    <footer id="footer"></footer>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="includes/navigation.js"></script>
+    <script src="includes/autosize.js"></script>
+    <script src="includes/playlist-embed-only.js"></script>
+    <script>
+        autosize( document.querySelectorAll( 'textarea' ) );
+        $( "#copy" ).click( function () {
+            $( "#link-container" ).select();
+            document.execCommand( 'copy' );
+        } );
+    </script>
+</body>
+
+</html>
