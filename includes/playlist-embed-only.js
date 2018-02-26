aa23a9637d75011dcda1fbf04cbb020e9de92b7c
haknudsen <paris_giovanni@yahoo.com>
2018-02-26 13:56:37 -0700

    removed unused var

--- includes/playlist-embed-only.js
+++ includes/playlist-embed-only.js
@@ -3,7 +3,6 @@ var center;
 var anchor = '<div style="width: 100%;max-width:1280px;margin:0 auto;padding:1rem">\n  <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">\n    <iframe type="text/html" style="position: absolute; top:0; left: 0; width: 100%; height: 100%" src="https://www.youtube.com/embed/videoseries?list=';
 var left = '<div style="width: 50%;max-width:640px;float:left;padding-right:1rem">\n  <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">\n    <iframe type="text/html" style="position: absolute; top:0; left: 0; width: 100%; height: 100%" src="https://www.youtube.com/embed/videoseries?list=';
 var frameEnd = '" frameborder="0"></iframe>\n  </div>\n</div>';
-var leftEnd = '\n  </div>\n</div>';
 
 $('#reset').click(function () {
 	"use strict";
