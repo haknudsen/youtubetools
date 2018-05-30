0fb3ffa4282b0b6b62a1ee0f8f116b5d5bee84fb
Andy Knudsen <andy@websitetalkingheads.com>
2018-05-30 17:49:35 -0600

    formated count-text
    working on video-sitemap

--- video-sitemap.php
+++ video-sitemap.php
@@ -1,9 +1,8 @@
 <!doctype html>
 <html>
-
 <head>
-	<title>YouTube Video Sitemap Creator</title>
-	<meta name="description" content="Create Video Sitemap of the YouTube Videos on a Page">
+	<meta charset="utf-8">
+	<title>Create Video Sitemap</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -13,43 +12,75 @@
 
 <body>
 	<?php include("includes/header.php"); ?>
-	<section class="jumbotron">
-		<h1 class="text-center">Create Video Sitemap of the YouTube Videos On A Page</h1>
-		<div id="login-container" class="pre-auth text-center"> This application requires access to your YouTube account. Please <a href="#" id="login-link">authorize</a> to continue. </div>
-		<hr>
-		<div class="row">
-			<div class="col-4">
-				<h3 class="text-center">Page to Check</h3>
-			  <input type="text" name="page" id="page" style="width: 100%">
-				<h3 class="text-center">Change Frequency</h3>
-				<input name="changefreq" type="text" id="changefreq" style="width: 100%;text-align: center" value="daily">
-				<h3 class="text-center">Priority</h3>
-			  <input name="priority" type="text" id="priority" style="width: 100%;text-align: center" value="0.8">
-			  <div class="d-flex align-items-center">
-				<button type="button" class="btn btn-purple center-block" id="submit">Submit</button>
+	<section class="container-fluid">
+		<div class="text-center">
+			<h1>Video Data Editor</h1>
+			<div class="container">
+				<h3 class="text-capitalize text-center">Enter Video ID</h3>
+				<div class="row">
+					<input type="text" id="video">
 				</div>
 			</div>
-			<div class="col-8">
-				<h3 class="text-center">Sitemap for this page</h3>
-				<textarea id="sitemap"></textarea>
+			<div class="btn-row">
+				<button type="button" class="btn btn-drkblue" id="execute-request-button">Authorize</button>
+				<button type="button" class="btn btn-danger" id="getVideo">Get Video</button>
+				<button type="button" class="btn btn-purple" id="update">Update</button>
 			</div>
+			<h3 id="reporter" class="text-capitalize text-center"></h3>
 		</div>
-	</section>
-	<section class="alert alert-info">
-		<div id="video-container" class="container"></div>
-		<div class="button-container">
-			<button id="prev-button" class="paging-button" onclick="previousPage();">Previous Page</button>
-			<button id="next-button" class="paging-button" onclick="nextPage();">Next Page</button>
+		<div class="alert alert-info">
+			<div class="row">
+				<div class="col-sm-6">
+					<h3 class="text-capitalize text-center">title</h3>
+					<input type="text" id="title">
+				</div>
+				<div class="col-sm-1">
+					<h3 class="text-capitalize text-center">category</h3>
+					<input type="text" id="category">
+				</div>
+				<div class="col-sm-1">
+					<h3 class="text-capitalize text-center">language</h3>
+					<input type="text" id="language">
+				</div>
+				<div class="col-sm-4">
+					<h3 class="text-capitalize text-center">Channel Title</h3>
+					<input type="text" id="channelTitle">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<h3 class="text-capitalize text-center">description</h3>
+					<textarea id="description" style="max-height: 960px;min-height: 20px"></textarea>
+					<div class="m-1">
+						<h3 class="text-capitalize text-center">Total Letter Count : <span id="counter">0</span></h3>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<h3 class="text-capitalize text-center">tags</h3>
+					<textarea id="tags" style="max-height: 960px;min-height: 20px"></textarea>
+					<h3 class="text-capitalize text-center">Total Letter Count : <span id="counter2">0</span></h3>
+				</div>
+			</div>
 		</div>
 	</section>
 	<?php include("includes/random-site-ad.php"); ?>
 	<?php include("includes/nav.html"); ?>
-	<script async defer src="https://apis.google.com/js/api.js" onload="this.onload=function(){};handleClientLoad()" onreadystatechange="if (this.readyState === 'complete') this.onload()">
-	<script type="text/javascript" src="includes/get-video-data.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="includes/autosize.js"></script>
+	<script src="includes/jquery.simplyCountable-description.js"></script>
+	<script async defer src="https://apis.google.com/js/api.js" onload="this.onload=function(){};handleClientLoad()" onreadystatechange="if (this.readyState === 'complete') this.onload()">
+	</script>
+	<script src="includes/get-video-data.js"></script>
 	<script>
-		autosize( document.querySelectorAll( 'textarea' ) );
+		$( document ).ready( function () {
+			autosize( document.querySelectorAll( 'textarea' ) );
+			$( '#description' ).simplyCountable();
+			$( '#tags' ).simplyCountable( {
+				counter: '#counter2',
+				countType: 'characters',
+				maxCount: 500
+			} );
+		} );
 	</script>
 </body>
-
 </html>
